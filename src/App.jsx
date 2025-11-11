1564152954dbdaf7786841de9c114c21264269d8
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-11 11:26:45 +0100

    add the array form films title and gener

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
-
+const moviesData = [
+  { title: "Inception", genre: "Fantascienza" },
+  { title: "Il Padrino", genre: "Thriller" },
+  { title: "Titanic", genre: "Romantico" },
+  { title: "Batman", genre: "Azione" },
+  { title: "Interstellar", genre: "Fantascienza" },
+  { title: "Pulp Fiction", genre: "Thriller" },
+];
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
